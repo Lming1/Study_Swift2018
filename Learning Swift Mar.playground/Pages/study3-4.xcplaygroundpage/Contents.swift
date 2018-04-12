f958931e0e54000c910bb3bba6eb5d4ff0dbed55
Lming1 <spear131121@gmail.com>
2018-04-13 01:26:47 +0900

    04/22 study

--- Learning Swift Mar.playground/Pages/study3-4.xcplaygroundpage/Contents.swift
+++ Learning Swift Mar.playground/Pages/study3-4.xcplaygroundpage/Contents.swift
@@ -20,3 +20,35 @@ let lz = LazyTest()
 lz.late
 
 // 클로저를 이용한 저장 프로퍼티 초기화
+
+class PropertyInit {
+    // 저장 프로퍼티 - 인스턴스 생성 시 최초 한 번만 실행.
+    var value01: String! = {
+        print("value execute")
+        return "value01"
+    }()
+    
+    let value02: String! = {
+        print("value02 execute")
+        return "value02"
+    }()
+    
+    // 프로퍼티 참조 시에 최초 한 번만 실행
+    lazy var value03: String! = {
+        print("value03 execute")
+        return "value03"
+    }()
+}
+
+
+let s = PropertyInit()
+s.value01
+s.value02
+
+let s1 = PropertyInit()
+s1.value03
+s1.value03
+
+
+
+
