9a2e11f9fd4f42592fddae4ce741538bf8940596
Lming1 <spear131121@gmail.com>
2018-05-28 01:56:19 +0900

    notification study. 0528

--- Msg-AlertController/Msg-AlertController/ViewController.swift
+++ Msg-AlertController/Msg-AlertController/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        // 간혹 메세지창을 처리해야할 때(사용자의 액션 없이도..)
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -24,12 +28,20 @@ class ViewController: UIViewController {
     @IBAction func alert(_ sender: Any) {
         // 메세지 창 객체
         let alert = UIAlertController(title: "Select", message: "Contents", preferredStyle: .alert)
-        let cancle = UIAlertAction(title: "cancle", style: .cancel, handler: nil)
-        let ok = UIAlertAction(title: "accept", style: .default, handler: nil)
-        let exec = UIAlertAction(title: "run", style: .destructive)
-        let stop = UIAlertAction(title: "stop", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "cancel", style: .cancel) { (_) in
+            self.result.text = "취소 버튼 클릭"
+        }
+        let ok = UIAlertAction(title: "accept", style: .default) { (_) in
+            self.result.text = "확인 버튼 클릭"
+        }
+        let exec = UIAlertAction(title: "run", style: .destructive) { (_) in
+            self.result.text = "실행 버튼 클릭"
+        }
+        let stop = UIAlertAction(title: "stop", style: .default) { (_) in
+            self.result.text = "중지 버튼 클릭"
+        }
         // 버튼 컨트롤러에 등록
-        alert.addAction(cancle)
+        alert.addAction(cancel)
         alert.addAction(ok)
         alert.addAction(exec)
         alert.addAction(stop)
@@ -37,6 +49,72 @@ class ViewController: UIViewController {
         self.present(alert, animated: false)
         
     }
+    
+    @IBAction func login(_ sender: Any) {
+        let title = "iTunes Store에 로그인"
+        let message = "사용자의 Apple ID hello@test.com의 암호를 입력하십시오"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in
+            //확인 버튼 클릭시 처리할 내용
+            if let tf = alert.textFields?.first {
+                print("입력된 값은 \( tf.text! )입니다.")
+            } else {
+                print("입력된 값이 없습니다.")
+            }
+        }
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        // text field add
+        
+        alert.addTextField(configurationHandler: { (tf) in
+            tf.placeholder = "암호"
+            tf.isSecureTextEntry = true
+        })
+        
+        self.present(alert, animated: false)
+        
+    }
+    
+    @IBAction func auth(_ sender: Any) {
+        let msg = "로그인"
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in
+            // 확인 버튼 클릭시
+            let loginId = alert.textFields?[0].text
+            let loginPass = alert.textFields?[1].text
+            
+            if loginId == "test" && loginPass == "test1234" {
+                self.result.text = "인증 성공"
+            } else {
+                self.result.text = "인증 실패"
+            }
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        // 아이디 필드
+        alert.addTextField(configurationHandler: {(tf) in
+            tf.placeholder = "아이디"
+            tf.isSecureTextEntry = false
+        })
+        
+        // 패스워드 필드
+        alert.addTextField(configurationHandler: {(tf) in
+            tf.placeholder = "비밀번호"
+            tf.isSecureTextEntry = true
+        })
+        
+        
+        self.present(alert, animated: false)
+        
+    }
+    
+    
+    
     
 }
 
---
+++ Msg-Notification/Msg-Notification/AppDelegate.swift
@@ -0,0 +1,106 @@
+//
+//  AppDelegate.swift
+//  Msg-Notification
+//
+//  Created by 이민혁 on 2018. 5. 28..
+//  Copyright © 2018년 이민혁. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 10.0, *) {
+            // 경고창, 배지, 사운드를 사용하는 알림 환경 정보를 생성하고, 사용자 동의 여부 창을 실행하겠다.
+            let notiCenter = UNUserNotificationCenter.current()
+            notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (didAllow, e) in }
+            notiCenter.delegate = self
+        } else {
+            // 경고창, 배지, 사운드를 사용하는 알림 환경 정보를 생성하고, 이를 어플리케이션에 저장
+            let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(setting)
+        }
+        return true
+    }
+    
+    // app 실행 도중에 알림 메시지가 도착한 경우
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if notification.request.identifier == "wakeup" {
+            let userInfo = notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+        // 알림 배너 띄워주기
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    // 사용자가 알림 메시지를 클릭했을 경우
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == "wakeup" {
+            let userInfo = response.notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+        completionHandler()
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if #available(iOS 10.0, *) { // UserNotification framework를 이용한 로컬 알림(ios 10이상)
+            //알림 동의 여부 확인
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == UNAuthorizationStatus.authorized {
+                    // 알림 콘텐츠 객체
+                    let nContent = UNMutableNotificationContent()
+                    nContent.badge = 1
+                    nContent.title = "로컬 알림 메시지"
+                    nContent.subtitle = "준비된 내용이 아주 많아요! 얼른 다시 앱을 열어주세요"
+                    nContent.body = "왜 나갔어요?? 어서 들어오세요!"
+                    nContent.sound = UNNotificationSound.default()
+                    nContent.userInfo = ["name": "ming"]
+                    
+                    // 알림 발송 조건 객체
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    
+                    // 알림 요청 객체
+                    let request = UNNotificationRequest(identifier: "wakeup", content: nContent, trigger: trigger)
+                    
+                    //notification center 추가
+                    UNUserNotificationCenter.current().add(request)
+                } else {
+                    print("사용자가 동의하지 않았어요!")
+                }
+            }
+        } else {
+            // UILocalNotification 객체를 이용한 로컬 알림(ios 9 이하)
+        }
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
