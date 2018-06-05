4bf6baa51a16cbe679b4bba57aa2152c704b73ba
Lming1 <spear131121@gmail.com>
2018-06-06 04:00:00 +0900

    navigation delegate - webkit

--- MyMovieChart/MyMovieChart/DetailViewController.swift
+++ MyMovieChart/MyMovieChart/DetailViewController.swift
@@ -11,17 +11,20 @@ import WebKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var wv: WKWebView! // Deploy target 신경쓰기..10.0 안되네
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     var mvo: MovieVO! // 목록 화면에서 전달하는 영화 정보를 받을 변수..
     
     override func viewDidLoad() {
+        // WKNavigationDelegate의 델리게이트 객체 지정
+        self.wv.navigationDelegate = self
         NSLog("linkurl = \(self.mvo.detail!), title =\(self.mvo.title!)")
         
         let navibar = self.navigationItem
         navibar.title = self.mvo.title
         
         // create URLRequest instance
-        if let url = self.mvo.detail {
+        if let url = self.mvo?.detail {
             if let urlObj = URL(string: url) {
                 let req = URLRequest(url: urlObj)
                 // call loadRequest method.
@@ -50,4 +53,47 @@ class DetailViewController: UIViewController {
     }
     
 
+}
+
+// MARK: - WKNavigationDelegate protocol
+extension DetailViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        self.spinner.startAnimating()
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.spinner.stopAnimating()
+    }
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.spinner.stopAnimating()
+        // 경고창
+        self.alert("상세 페이지를 읽어오지 못했습니다.") {
+            // 버튼 클릭시 이전 화면으로 복귀
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+//        let alert = UIAlertController(title: "오류", message: "상세 페이지를 읽어오지 못했습니다", preferredStyle: .alert)
+//        let cancelAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+//            _ = self.navigationController?.popViewController(animated: true)
+//        }
+//        alert.addAction(cancelAction)
+//        self.present(alert, animated: false, completion: nil)
+    }
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        self.spinner.stopAnimating()
+        self.alert("상세 페이지를 읽어오지 못했습니다.") {
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
+
+// MARK: - simple alert extension
+extension UIViewController {
+    func alert(_ message: String, onClick: (() -> Void)? = nil) {
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel){ (_) in
+            onClick?()
+        })
+        DispatchQueue.main.async {
+            self.present(controller, animated: false)
+        }
+    }
 }
