2d74cbb0e2514f8c0de9eea14732608948789672
Lming1 <spear131121@gmail.com>
2018-06-05 16:09:55 +0900

    JSON parse

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -7,33 +7,82 @@
 //
 
 import UIKit
+import Foundation
 
 class ListViewController: UITableViewController {
-    //튜플 아이템을 가진 배열로 정의된 데이터 셋
-    var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
-    ]
+    var page = 1
+    @IBOutlet var moreBtn: UIButton!
+//    //튜플 아이템을 가진 배열로 정의된 데이터 셋
+//    var dataset = [
+//        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+//        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+//        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
+//    ]
+    
     
     //테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            
-            datalist.append(mvo)
-        }
+//        for (title, desc, opendate, rating, thumbnail) in self.dataset {
+//            let mvo = MovieVO()
+//            mvo.title = title
+//            mvo.description = desc
+//            mvo.opendate = opendate
+//            mvo.rating = rating
+//            mvo.thumbnail = thumbnail
+//
+//            datalist.append(mvo)
+//        }
         return datalist
     }()
     
     override func viewDidLoad() {
+        self.callMovieAPI()
+    }
+    
+    func callMovieAPI() {
+        // get uri
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        // rest api call
+        let apidata = try! Data(contentsOf: apiURI)
+        // log
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "Data 없어요.."
+        NSLog("API Result = \(log)")
         
+        // JSON 객체 파싱 NSDictionary 객체로 받음
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            // 데이터 구조에 따라 차례대로 캐스팅
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            // Iterator 처리, API 데이터를 MovieVO객체 저장
+            for row in movie {
+                // 순회 상수를 딕셔너리 타입으로 캐스팅
+                let r = row as! NSDictionary
+                // 테이블 뷰 리스트를 구성할 데이터 형식
+                let mvo = MovieVO()
+                // 무비 배열의 각 데이터를 mvo 상수의 속성에 대입
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                // list 배열에 추가
+                self.list.append(mvo)
+            }
+            // 전체 데이터 카운트를 얻는다.
+            let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+            // totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
+            if (self.list.count >= totalCount) {
+                self.moreBtn.isHidden = true
+            }
+        } catch{
+            NSLog("Parse Error!!")
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,7 +97,19 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        // thumbnail local 처리
+//        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        
+        
+        // 섬네일 경로를 인자값으로 하는 URL 객체 생성
+        let url: URL! = URL(string: row.thumbnail!)
+        // 이미지를 읽어와 Data 객체 저장
+        let imageData = try! Data(contentsOf: url)
+        // UIImage 객체를 생성하여 아울렛 변수의 Image 속성에 대입
+        cell.thumbnail.image = UIImage(data: imageData)
+        
+//        cell.thumbnail.image = UIImage(data: try! Data(contentsOf: URL(string: row.thumbnail!)!))
+        
 //        let title = cell.viewWithTag(101) as? UILabel
 //        let desc = cell.viewWithTag(102) as? UILabel
 //        let opendate = cell.viewWithTag(103) as? UILabel
@@ -69,5 +130,12 @@ class ListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
+    }
+    
+    
+    @IBAction func more(_ sender: Any) {
+        self.page += 1
+        self.callMovieAPI()
+        self.tableView.reloadData()
     }
 }
