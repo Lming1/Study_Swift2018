9c045844ebe1a7affa9b21806bd6a2498045590d
Lming1 <spear131121@gmail.com>
2018-06-10 04:32:12 +0900

    memoreadVC & app icon add

--- Memo/Memo/MemoReadVC.swift
+++ Memo/Memo/MemoReadVC.swift
@@ -9,27 +9,25 @@
 import UIKit
 
 class MemoReadVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var param : MemoData?
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var img: UIImageView!
+    
+    override func viewDidLoad() {
+        // 제목, 내용, 이미지 출력
+        self.subject.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        // 날짜 포맷 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dateString = formatter.string(from: (param?.regdate)!)
+        
+        // navigation title에 날짜 표시
+        self.navigationItem.title = dateString
     }
-    */
 
 }
