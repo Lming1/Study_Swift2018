2a623186fb40646eb9263f6d16d8c3e321f3b054
Lming1 <spear131121@gmail.com>
2018-06-18 17:38:47 +0900

    font 추가 및 x좌표 통일

--- InputForm/InputForm/ViewController.swift
+++ InputForm/InputForm/ViewController.swift
@@ -20,24 +20,25 @@ class ViewController: UIViewController {
         // navigation bar title setting
         self.navigationItem.title = "Setting"
         // create email label(frame, text)
+        let customFont = UIFont(name: "SDMiSaeng", size: 14)
         let lblEmail = UILabel()
         lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
         lblEmail.text = "이메일"
         // label font setting
-        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        lblEmail.font = customFont
         // label add root view
         self.view.addSubview(lblEmail)
         
         let lblUpdate = UILabel()
-        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.frame = CGRect(x: lblEmail.frame.origin.x, y: 150, width: 100, height: 30)
         lblUpdate.text = "자동갱신"
-        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        lblUpdate.font = customFont
         self.view.addSubview(lblUpdate)
         
         let lblInterval = UILabel()
-        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.frame = CGRect(x: lblEmail.frame.origin.x, y: 200, width: 100, height: 30)
         lblInterval.text = "갱신주기"
-        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        lblInterval.font = customFont
         self.view.addSubview(lblInterval)
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -49,12 +50,12 @@ class ViewController: UIViewController {
         self.view.addSubview(self.paramEmail)
         
         self.paramUpdate = UISwitch()
-        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        self.paramUpdate.frame = CGRect(x: self.paramEmail.frame.origin.x, y: 150, width: 50, height: 30)
         self.paramUpdate.setOn(true, animated: true)
         self.view.addSubview(self.paramUpdate)
         
         self.paramInterval = UIStepper()
-        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.frame = CGRect(x: self.paramEmail.frame.origin.x, y: 200, width: 50, height: 30)
         self.paramInterval.minimumValue = 0
         self.paramInterval.maximumValue = 100
         self.paramInterval.stepValue = 1
@@ -69,7 +70,7 @@ class ViewController: UIViewController {
         self.view.addSubview(self.txtUpdate)
         
         self.txtInterval = UILabel()
-        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.frame = CGRect(x: self.txtUpdate.frame.origin.x, y: 200, width: 100, height: 30)
         self.txtInterval.font = UIFont.systemFont(ofSize: 12)
         self.txtInterval.textColor = UIColor.red
         self.txtInterval.text = "0분마다"
@@ -77,6 +78,21 @@ class ViewController: UIViewController {
         
         self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
         self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
+        
+        let submitBtn = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(submit(_:)))
+        self.navigationItem.rightBarButtonItem = submitBtn
+        
+        
+        // 폰트 파일 Post Script name 확인
+        
+        for family in UIFont.familyNames {
+            print("\(family)")
+            
+            for names in UIFont.fontNames(forFamilyName: family) {
+                print("== \(names)")
+            }
+        }
+        
     }
 
     // 스위치와 상호 반응할 액션 메소드
@@ -86,6 +102,15 @@ class ViewController: UIViewController {
     
     @objc func presentIntervalValue(_ sender: UIStepper) {
         self.txtInterval.text = ("\(Int(sender.value))분마다")
+    }
+    
+    @objc func submit(_ sender: Any) {
+        let rvc = ReadViewController()
+        rvc.pEmail = self.paramEmail.text
+        rvc.pUpdate = self.paramUpdate.isOn
+        rvc.pInterval = self.paramInterval.value
+        
+        self.navigationController?.pushViewController(rvc, animated: true)
     }
 
 }
