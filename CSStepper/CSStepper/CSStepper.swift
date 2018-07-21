54ee27868bab4b81fac65499d875082f17db627e
Lming1 <spear131121@gmail.com>
2018-07-21 22:55:39 +0900

    storyboard 커스터마이징을 위해 @IBInspectable 추가

--- CSStepper/CSStepper/CSStepper.swift
+++ CSStepper/CSStepper/CSStepper.swift
@@ -15,11 +15,44 @@ public class CSStepper: UIView {
     public var leftBtn = UIButton(type: .system)
     public var rightBtn = UIButton(type: .system)
     public var centerLabel = UILabel()
+    @IBInspectable
     public var value: Int = 0 {
         didSet { // 프로퍼티 값 변경 시 자동 호출
             self.centerLabel.text = String(value)
         }
     }
+    
+    // 왼쪽 타이틀
+    @IBInspectable
+    public var leftTitle: String = "⬇︎" {
+        didSet {
+            self.leftBtn.setTitle(leftTitle, for: .normal)
+        }
+    }
+    
+    // 오른쪽 타이틀
+    @IBInspectable
+    public var rightTitle: String = "⬆︎" {
+        didSet {
+            self.rightBtn.setTitle(rightTitle, for: .normal)
+        }
+    }
+    
+    // label 컬러
+    @IBInspectable
+    public var bgColor: UIColor = UIColor.cyan {
+        didSet {
+            self.centerLabel.backgroundColor = backgroundColor
+        }
+    }
+    
+    // 증감값 단위
+    @IBInspectable
+    public var stepValue: Int = 1
+    // 최대, 최소 값
+    @IBInspectable public var maximumValue: Int = 100
+    @IBInspectable public var minimumValue: Int = -100
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -48,13 +81,13 @@ public class CSStepper: UIView {
         
         
         self.leftBtn.tag = -1
-        self.leftBtn.setTitle("⬇︎", for: .normal)
+        self.leftBtn.setTitle(self.leftTitle, for: .normal)
         self.leftBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         self.leftBtn.layer.borderWidth = borderWidth
         self.leftBtn.layer.borderColor = borderColor
         
         self.rightBtn.tag = 1
-        self.rightBtn.setTitle("⬆︎", for: .normal)
+        self.rightBtn.setTitle(self.rightTitle, for: .normal)
         self.rightBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         self.rightBtn.layer.borderWidth = borderWidth
         self.rightBtn.layer.borderColor = borderColor
@@ -62,7 +95,7 @@ public class CSStepper: UIView {
         self.centerLabel.text = String(value)
         self.centerLabel.font = UIFont.systemFont(ofSize: 16)
         self.centerLabel.textAlignment = .center
-        self.centerLabel.backgroundColor = UIColor.cyan
+        self.centerLabel.backgroundColor = self.bgColor
         self.centerLabel.layer.borderWidth = borderWidth
         self.centerLabel.layer.borderColor = borderColor
         
@@ -87,7 +120,17 @@ public class CSStepper: UIView {
     
     // value 속성 값 변경
     @objc public func valueChange(_ sender: UIButton) {
-        self.value += sender.tag
+        // 값 체크. 최대값 & 최소값 범위를 벗어날 경우 종료
+        let sum = self.value + (sender.tag * self.stepValue)
+        if sum > self.maximumValue {
+            return
+        }
+        if sum < self.minimumValue {
+            return
+        }
+        
+        
+        self.value += (sender.tag * self.stepValue)
     }
 
 }
