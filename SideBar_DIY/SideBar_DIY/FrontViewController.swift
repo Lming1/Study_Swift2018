02377e128f0a94a7831d44dca98ef128718ff4dd
Lming1 <spear131121@gmail.com>
2018-07-26 01:54:35 +0900

    제스처 등록

--- SideBar_DIY/SideBar_DIY/FrontViewController.swift
+++ SideBar_DIY/SideBar_DIY/FrontViewController.swift
@@ -20,15 +20,37 @@ class FrontViewController: UIViewController {
         let btnSideBar = UIBarButtonItem(image: UIImage(named: "sidemenu.png"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(moveSide))
         // 버튼을 내비게이션 바의 왼쪽 영역에 추가
         self.navigationItem.leftBarButtonItem = btnSideBar
+        
+        // 화면 끝 -> 다른 쪽 패닝 제스처 정의
+        let dragLeft = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(moveSide(_:)))
+        dragLeft.edges = UIRectEdge.left // 시작 모서리 : 왼쪽
+        self.view.addGestureRecognizer(dragLeft) // 뷰에 제스처 등록하기
+        
+        // 화면 스와이프 제스처 정의
+        let dragRight = UISwipeGestureRecognizer(target: self, action: #selector(moveSide(_:)))
+        dragRight.direction = .left // 왼쪽 방향
+        self.view.addGestureRecognizer(dragRight)
     }
     
     // 사용자 액션에 반응하여 델리게이트 메소드 호출
     @objc func moveSide(_ sender: Any) {
         NSLog("button test")
-        if self.delegate?.isSideBarShowing == false {
-            self.delegate?.openSideBar(nil) // 사이드 바를 연다
-        } else {
-            self.delegate?.closeSideBar(nil) // 닫는다
+//        if self.delegate?.isSideBarShowing == false {
+//            self.delegate?.openSideBar(nil) // 사이드 바를 연다
+//        } else {
+//            self.delegate?.closeSideBar(nil) // 닫는다
+//        }
+        
+        if sender is UIScreenEdgePanGestureRecognizer {
+            self.delegate?.openSideBar(nil)
+        } else if sender is UISwipeGestureRecognizer {
+            self.delegate?.closeSideBar(nil)
+        } else if sender is UIBarButtonItem {
+            if self.delegate?.isSideBarShowing == false {
+                self.delegate?.openSideBar(nil)
+            } else {
+                self.delegate?.closeSideBar(nil)
+            }
         }
     }
 
