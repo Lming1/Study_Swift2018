28c1278e4fc4198a7db39dc79e2f089d1ab1348a
Lming1 <spear131121@gmail.com>
2018-08-04 00:51:09 +0900

    user info management 프로퍼티

--- Memo/Memo/UserInfoManager.swift
+++ Memo/Memo/UserInfoManager.swift
@@ -19,5 +19,56 @@ struct UserInfoKey {
 
 
 class UserInfoManager {
+    var loginid: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: UserInfoKey.loginId)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.loginId)
+            ud.synchronize()
+        }
+    }
     
+    var account: String?  {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.account)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.account)
+            ud.synchronize()
+        }
+    }
+    
+    var name: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.name)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.name)
+            ud.synchronize()
+        }
+    }
+    
+    var profile: UIImage? {
+        get {
+            let ud = UserDefaults.standard
+            if let _profile = ud.data(forKey: UserInfoKey.profile) {
+                return UIImage(data: _profile)
+            } else {
+                return UIImage(named: "account.jpg")
+            }
+        } set(v) {
+            if v != nil {
+                let ud = UserDefaults.standard
+                // UIImagePNGRepresentation = UIImage 타입의 객체를 데이터 유실 없이 Data 타입으로 변환
+                ud.set(UIImagePNGRepresentation(v!), forKey: UserInfoKey.profile)
+                ud.synchronize()
+            }
+        }
+    }
+    
+    // 로그인 상태
 }
