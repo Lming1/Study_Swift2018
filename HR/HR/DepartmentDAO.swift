14cdcd33495383149c470c88fa056d17b0894e97
Lming1 <spear131121@gmail.com>
2018-08-22 00:34:58 +0900

    department dao

--- HR/HR/DepartmentDAO.swift
+++ HR/HR/DepartmentDAO.swift
@@ -35,4 +35,91 @@ class DepartmentDAO {
         let db = FMDatabase(path: dbPath)
         return db
     }()
+    
+    init() {
+        self.fmdb.open()
+    }
+    deinit {
+        self.fmdb.close()
+    }
+    
+    func find() -> [DepartRecord] {
+        // 반환할 데이터를 담을 [DepartRecord] 타입의 객체 정의
+        var departList = [DepartRecord]()
+        
+        do {
+            // 부서 목록 sql
+            let sql = """
+             SELECT depart_cd, depart_title, depart_addr FROM department ORDER BY depart_cd ASC
+            """
+            
+            let rs = try self.fmdb.executeQuery(sql, values: nil)
+            
+            // 결과 추출
+            while rs.next() {
+                let departCd = rs.int(forColumn: "depart_cd")
+                let departTitle = rs.string(forColumn: "depart_title")
+                let departAddr = rs.string(forColumn: "depart_addr")
+                
+                departList.append((Int(departCd), departTitle!, departAddr!))
+            }
+        } catch let error as NSError  {
+            print("failed: \(error.localizedDescription)")
+        }
+        return departList
+    }
+    
+    func get(departCd: Int) -> DepartRecord? {
+        // 질의
+        let sql = """
+            SELECT depart_cd, depart_title, depart_addr FROM department WHERE depart_cd = ?
+        """
+        
+        let rs = self.fmdb.executeQuery(sql, withArgumentsIn: [departCd])
+        
+        // 결과 추출
+        if let _rs = rs { // 결과 => 옵셔널 타입으로 반환됨, 일반 상수에 바인딩하여 해제해야함
+            _rs.next()
+            
+            let departId = _rs.int(forColumn: "depart_cd")
+            let departTitle = _rs.string(forColumn: "depart_title")
+            let departAddr = _rs.string(forColumn: "depart_addr")
+            
+            return (Int(departId), departTitle!, departAddr!)
+        } else {
+            return nil
+        }
+    }
+    
+    func create(title: String!, addr: String!) -> Bool {
+        guard title != nil && title?.isEmpty == false else {
+            return false
+        }
+        guard addr != nil && addr?.isEmpty == false else {
+            return false
+        }
+        
+        do {
+            let sql = """
+                INSERT INTO department(depart_title, depart_addr)
+                VALUES (?, ?)
+                """
+            try self.fmdb.executeUpdate(sql, values: [title, addr])
+            return true
+        } catch let error as NSError {
+            print("Insert Error : \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    func remove(departCd: Int) -> Bool {
+        do {
+            let sql = "DELETE FROM department WHERE depart_cd= ? "
+            try self.fmdb.executeUpdate(sql, values: [departCd])
+            return true
+        } catch let error as NSError {
+            print("DELETE Error : \(error.localizedDescription)")
+            return false
+        }
+    }
 }
