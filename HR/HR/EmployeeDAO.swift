81f2d624eabd0d323d3be2f84e155567621c7045
Lming1 <spear131121@gmail.com>
2018-08-22 22:13:53 +0900

    employeeDAO CRUD

--- HR/HR/EmployeeDAO.swift
+++ HR/HR/EmployeeDAO.swift
@@ -54,5 +54,105 @@ class EmployeeDAO {
         self.fmdb.close()
     }
     
+    // 전체 리스트
+    func find() -> [EmployeeVO] {
+        var employeeList = [EmployeeVO]()
+        
+        do {
+            let sql = """
+                SELECT emp_cd, emp_name, join_date, state_cd, department.depart_title
+                FROM employee
+                JOIN department On department.depart_cd = employee.depart_cd
+                ORDER BY empleyee.depart_cd ASC
+                """
+            
+            let rs = try self.fmdb.executeQuery(sql, values: nil)
+            
+            while rs.next() {
+                var record = EmployeeVO()
+                
+                record.empCd = Int(rs.int(forColumn: "emp_cd"))
+                record.empName = rs.string(forColumn: "emp_name")!
+                record.joinDate = rs.string(forColumn: "join_date")!
+                record.departTitle = rs.string(forColumn: "depart_title")!
+                
+                let cd = Int(rs.int(forColumn: "state_cd")) // DB에서 읽어온 state_cd
+                record.stateCd = EmpStateType(rawValue: cd)!
+                
+                employeeList.append(record)
+            }
+        } catch let error as NSError {
+            print("failed: \(error.localizedDescription)")
+        }
+        return employeeList
+    }
+    
+    // 단일 레코드
+    func get(empCd: Int) -> EmployeeVO? {
+        // 질의
+        let sql = """
+            SELECT emp_cd, emp_name, join_date, state_cd, department.depart_title
+            FROM employee
+            JOIN department On department.depart_cd = employee.depart_cd
+            WHERE emp_cd = ?
+            """
+        
+        let rs = self.fmdb.executeQuery(sql, withArgumentsIn: [empCd])
+        // 결과 처리
+        
+        if let _rs = rs {
+            _rs.next()
+            
+            var record = EmployeeVO()
+            record.empCd = Int(_rs.int(forColumn: "emp_cd"))
+            record.empName = _rs.string(forColumn: "emp_name")!
+            record.joinDate = _rs.string(forColumn: "join_date")!
+            record.departTitle = _rs.string(forColumn: "depart_title")!
+            
+            let cd = Int(_rs.int(forColumn: "state_cd"))
+            record.stateCd = EmpStateType(rawValue: cd)!
+            
+            return record
+        } else  {
+            return nil
+        }
+        
+    }
+    
+    // create
+    func create(param: EmployeeVO) -> Bool {
+        do {
+            let sql = """
+                INSERT INTO employee (emp_name, join_date, state_cd, depart_cd)
+                VALUES( ?, ?, ?, ? )
+                """
+            
+            // Prepared Statement를 위한 인자값 정의
+            var params = [Any]()
+            params.append(param.empName)
+            params.append(param.joinDate)
+            params.append(param.stateCd.rawValue)
+            params.append(param.departCd)
+            
+            try self.fmdb.executeUpdate(sql, values: params)
+            return true
+        } catch let error as NSError {
+            print("Insert ERROR : \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    func remove(empCd: Int) -> Bool {
+        do {
+            let sql = "DELETE FROM employee WHERE emp_cd = ? "
+            try self.fmdb.executeUpdate(sql, values: [empCd])
+            return true
+        } catch let error as NSError {
+            print("DELETE Error : \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    
     
 }
