2659b8f54f2d7ca3672053807c1f07714c9bdf8e
Lming1 <spear131121@gmail.com>
2018-09-26 02:03:33 +0900

    Memo App CoreData init

---
+++ S_ManualCoreData/S_ManualCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  S_ManualCoreData
+//
+//  Created by 이민혁 on 2018. 9. 26..
+//  Copyright © 2018년 이민혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
