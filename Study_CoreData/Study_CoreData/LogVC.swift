6d9571db3f27a874526f2ccdeaa650ec814d4be3
Lming1 <spear131121@gmail.com>
2018-09-25 23:57:46 +0900

    logVC init

---
+++ Study_CoreData/Study_CoreData/LogVC.swift
@@ -0,0 +1,14 @@
+//
+//  LogVC.swift
+//  Study_CoreData
+//
+//  Created by 이민혁 on 2018. 9. 25..
+//  Copyright © 2018년 이민혁. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogVC: UITableViewController {
+    
+}
