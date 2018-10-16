e6a2793c11bd0fe9ce1b0e992460658227db84d9
Lming1 <spear131121@gmail.com>
2018-10-16 18:25:16 +0900

    cocoa json example

--- APITest/APITest/ViewController.swift
+++ APITest/APITest/ViewController.swift
@@ -76,6 +76,52 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    
+    @IBAction func json(_ sender: Any) {
+        let userId = (self.userId.text)!
+        let name = (self.name.text)!
+        
+        let param = ["userId" : userId, "name" : name]
+        let paramData = try! JSONSerialization.data(withJSONObject: param, options: [])
+        
+        let url = URL(string: "http://")
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = paramData
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(String(paramData.count), forHTTPHeaderField: "Content-Length")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let e = error {
+                NSLog("An error has occurred : \(e.localizedDescription)")
+                return
+            }
+            
+            DispatchQueue.main.async() {
+                do {
+                    let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+                    
+                    guard let jsonObject = object else { return }
+                    
+                    let result = jsonObject["result"] as? String
+                    let timeStamp = jsonObject["timestamp"] as? String
+                    let userId = jsonObject["userId"] as? String
+                    let name = jsonObject["name"] as? String
+                    if result == "SUCCESS" {
+                        self.responseView.text = "ID : \(userId!)" + "\n" + "이름 : \(name!)" + "\n" + "응답결과 : \(result!)" + "\n" + "응답시간 : \(timeStamp!)" + "\n" + "요청방식 : application/json"
+                    }
+                } catch let e as NSError {
+                    print("An error has occurred while parsing JSONObject : \(e.localizedDescription)")
+                }
+            }
+            
+        }
+        task.resume()
+    }
+    
 
 }
 
